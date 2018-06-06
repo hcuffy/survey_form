d001e4570c30213c9b4560da68d0117849db486b
Henry Cuffy <hcuffy_5@hotmail.com>
2018-06-06 14:22:45 +0200

    Clean up.

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const path = require ('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/')));
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
+
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
 
 app.listen(port, () => {
 console.log('Express server has started on', port);
